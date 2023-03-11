b64482618bbc05762487e1e52a5ba7257ef19d31
Roman Kozhin <kozhin.rm@gmail.com>
2023-03-11 10:23:42 +0300

    Кол-во пар соседних элементов

---
+++ cw_2/NumPairsNeighElem.java
@@ -0,0 +1,26 @@
+package cw_2;
+
+import java.util.Scanner;
+
+/*
+ * Дан массив целых чисел. Найти количество пар соседних элементов,
+ * где первый элемент вдвое больше второго.
+ */
+public class NumPairsNeighElem {
+    public static void main(String[] args) {
+        int k = 0;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] mass = new int[n];
+        for (int i = 0; i < n; i++) {
+            mass[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n-1; i++) {
+            if (mass[i] == 2 * mass[i+1]) {
+                k++;
+            }
+        }
+        scanner.close();
+        System.out.println("k = " + k);
+    }
+}
